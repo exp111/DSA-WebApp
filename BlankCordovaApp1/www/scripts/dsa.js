eb3ed5168e0b68ab2e1a3968f5ddb7a52a02e739
Exp <theexp111@gmail.com>
2017-06-12 15:03:23 +0200

    Updated everything

---
+++ BlankCordovaApp1/www/scripts/dsa.js
@@ -0,0 +1,23 @@
+﻿window.onload = function () {
+
+    //Save & Edit functions
+    var editlink = document.getElementById("editLink");
+    var savelink = document.getElementById("saveLink");
+
+    editlink.onclick = function () {
+        //Reset Links
+        document.getElementById("editLinkLi").style.display = "none";
+        document.getElementById("saveLinkLi").style.display = "block";
+        //Disable write lock on the input fields
+        return false;
+    }
+
+    savelink.onclick = function () {
+        //Reset Links
+        document.getElementById("saveLinkLi").style.display = "none";
+        document.getElementById("editLinkLi").style.display = "block";
+        //Enable write lock again
+        //Save changes to local storage
+        return false;
+    }
+}
---
+++ BlankCordovaApp1/www/scripts/offline.js
@@ -0,0 +1,11 @@
+﻿var window, global = (function () { return this; }());
+/*global global: true */
+(function () {
+    "use strict";
+
+    var appCache = window.applicationCache;
+    appCache.update();
+    if (appCache.status == window.applicationCache.UPDATEREADY) {
+        window.location.reload();
+    }
+}());
